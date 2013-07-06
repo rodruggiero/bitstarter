87ad9edad9be6ea035a36b8d1b7a25f7c67a5ad4
Ubuntu <ubuntu@ip-172-31-30-93.us-west-2.compute.internal>
2013-07-06 21:17:07 +0000

    Newest grader.js

--- grader.js
+++ grader.js
@@ -25,7 +25,7 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json");
+var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,7 +61,7 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module {
+if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
