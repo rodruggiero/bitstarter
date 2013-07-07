4fe6055ee7bb04c85ed6139f42ba456136cd2f97
Ubuntu <ubuntu@ip-172-31-30-93.us-west-2.compute.internal>
2013-07-07 02:28:17 +0000

    Again

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
   var message = fs.readFileSync('index.html')
-    response.send(buffer.toString(message));
+    response.send(message);
 });
 
 var port = process.env.PORT || 5000;
