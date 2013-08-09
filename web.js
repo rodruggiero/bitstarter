6a6cedeb131b0bb45e322f660e641e201e3c12f5
rodruggiero <rodruggiero@gmail.com>
2013-08-09 03:11:02 +0000

    Edited column width

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + './')); // necessary for local .css files
 
 var fs = require('fs');
 
