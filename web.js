677cbe16b96bedcf9a8fd6a88e8efcd2c4f3839f
rodruggiero <rodruggiero@gmail.com>
2013-08-30 07:21:33 +0000

    new pages

--- web.js
+++ web.js
@@ -10,6 +10,25 @@ app.get('/', function(request, response) {
     response.send(message.toString());
 });
 
+var about_html = 'about.html';
+
+app.get('/about', function(req, res) {
+  res.send(fs.readFileSync(about_html).toString());
+});
+
+var contact_html = 'contact.html';
+
+app.get('/contact', function(req, res) {
+  res.send(fs.readFileSync(contact_html).toString());
+});
+
+var soon_html = 'soon.html';
+
+app.get('/soon', function(req, res) {
+  res.send(fs.readFileSync(soon_html).toString());
+});
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
